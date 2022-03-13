fe01276cef3abe2e74aabd35fbfade05ece9c6cd
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-13 20:42:42 +0000

    added Client instance live that connects to the API

--- Sources/ScoreClientLive/Live.swift
+++ Sources/ScoreClientLive/Live.swift
@@ -1,8 +1,24 @@
-//
-//  File.swift
-//  
-//
-//  Created by Evans Domina Attafuah on 13/03/2022.
-//
-
 import Foundation
+import ScoreClient
+import WordleKit
+import Combine
+
+#warning("add ACCESS_TOKEN")
+//extension String {
+//static let accessToken = "ACCESS_TOKEN GOES HERE"
+//}
+
+
+public extension WordleKit.ScoreClient {
+    static var live: Self {
+        .init {
+            return URLSession.shared
+                .dataTaskPublisher(for: .scores())
+                .map(\.data)
+                .decode(type: WordleKit.Scores.self, decoder: JSONDecoder())
+                .receive(on: DispatchQueue.main)
+                .eraseToAnyPublisher()
+            
+        }
+    }
+}
