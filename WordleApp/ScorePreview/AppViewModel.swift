808a956d1be4fc2c67d18871a80abacde7c1cde9
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-13 20:47:11 +0000

    added a layman's technic for polling a service

--- WordleApp/ScorePreview/AppViewModel.swift
+++ WordleApp/ScorePreview/AppViewModel.swift
@@ -6,6 +6,7 @@ import SwiftUI
 public extension WordleKit {
     class AppViewModel: ObservableObject {
         @Published public var scores = [Scores.Score]()
+        @Published private var counter = 0
         private let scoreClient: ScoreClient
         private var cancellables = Set<AnyCancellable>()
         
@@ -13,9 +14,22 @@ public extension WordleKit {
             self.scoreClient = scoreClient
             polling()
             
+            Timer.publish(every: 10, on: .main, in: .common)
+                  .autoconnect()
+                  .scan(0) { count, _ in
+                      self.polling()
+                      return count + 1
+                  }
+                  .assign(to: \.counter, on: self)
+                  .store(in: &cancellables)
 
         }
         
+        private func difference(
+            local: [Scores.Score],
+            remote: [Scores.Score]) -> [Scores.Score] {
+            local.diff(from: remote)
+        }
         
         private func polling() {
             scoreClient
@@ -25,6 +39,11 @@ public extension WordleKit {
                     receiveValue: { [weak self] scores in
                         print("polling")
   
+                        dump(self?.difference(
+                                local: AppStorage.shared.scores,
+                                remote: scores.scores)
+                            )
+                            self?.scores = AppStorage.shared.scores
                       
                     }
                 )
