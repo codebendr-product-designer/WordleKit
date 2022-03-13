1c7c5ebbdb7f4ff015d71bfcaaf41f4a2233528a
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-13 03:00:56 +0000

    added Score Preview for quick prototyping

---
+++ WordleApp/ScorePreview/ScorePreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScorePreviewApp.swift
+//  ScorePreview
+//
+//  Created by Evans Domina Attafuah on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScorePreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
