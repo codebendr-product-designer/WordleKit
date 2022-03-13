7c71903663a67b7fb1b38ae9935382266aee16fb
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-13 01:36:27 +0000

    added Modules for Score Client to handshake with server

--- Package.swift
+++ Package.swift
@@ -6,21 +6,15 @@ import PackageDescription
 let package = Package(
     name: "WordleKit",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "WordleKit",
-            targets: ["WordleKit"]),
+        .library(name: "WordleKit", targets: ["WordleKit"]),
+        .library(name: "ScoreClient", targets: ["ScoreClient"]),
+        .library(name: "ScoreClientLive", targets: ["ScoreClientLive"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [ ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "WordleKit",
-            dependencies: []),
+        .target(name: "WordleKit"),
+        .target(name: "ScoreClient", dependencies: ["WordleKit"]),
+        .target(name: "ScoreClientLive", dependencies: ["WordleKit", "ScoreClient"]),
         .testTarget(
             name: "WordleKitTests",
             dependencies: ["WordleKit"]),
---
+++ Sources/ScoreClient/Interface.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Evans Domina Attafuah on 13/03/2022.
+//
+
+import Foundation
