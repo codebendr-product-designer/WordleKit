e7311818da937a23aecfbb75631360667110720b
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-13 01:34:46 +0000

    removed generated comments

--- WordleApp/WordleApp/ViewController.swift
+++ WordleApp/WordleApp/ViewController.swift
@@ -1,19 +1,10 @@
-//
-//  ViewController.swift
-//  WordleApp
-//
-//  Created by Evans Domina Attafuah on 13/03/2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
