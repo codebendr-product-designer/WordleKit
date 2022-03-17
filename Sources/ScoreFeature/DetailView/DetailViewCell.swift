d8a982e6e21de2b3bc79b26837dd16cb6fd65d02
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-17 15:22:25 +0000

    added DetailViewController & View Cell

---
+++ Sources/ScoreFeature/DetailView/DetailViewCell.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+public class DetailViewCell: UICollectionViewCell {
+    let label = UILabel()
+    static let reuseIdentifier = "text-cell-reuse-identifier"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("not implemnted")
+    }
+    
+}
+
+extension DetailViewCell {
+    func configure() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.adjustsFontForContentSizeCategory = true
+        contentView.addSubview(label)
+        let inset = CGFloat(10)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: inset),
+            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -inset),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: inset),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -inset),
+        ])
+    }
+}
--- Sources/ScoreFeature/DetailView/DetailViewController.swift
+++ Sources/ScoreFeature/DetailView/DetailViewController.swift
@@ -1,8 +1,27 @@
-//
-//  File.swift
-//  
-//
-//  Created by Evans Domina Attafuah on 17/03/2022.
-//
+import UIKit
 
-import Foundation
+class DetailViewController: UIViewController {
+    private var tries: [Tries]
+    
+    enum Section {
+        case main
+    }
+    
+    init(tries: [Tries]) {
+        self.tries = tries
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var dataSource: UICollectionViewDiffableDataSource<Section, Tries>! = nil
+    var collectionView: UICollectionView! = nil
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        configureHierarchy()
+//        configureDataSource()
+    }
+}
