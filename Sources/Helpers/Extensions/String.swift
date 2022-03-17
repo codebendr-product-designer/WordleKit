071f27c1cb53058734dcf4be95a8b5f19bbc92f6
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-17 12:25:20 +0000

    added some string extensions to add in detecting guess vs actual words

--- Sources/Helpers/Extensions/String.swift
+++ Sources/Helpers/Extensions/String.swift
@@ -1,8 +1,33 @@
-//
-//  File.swift
-//  
-//
-//  Created by Evans Domina Attafuah on 17/03/2022.
-//
 
-import Foundation
+public extension String {
+    func index(of char: Character) -> Int? {
+        firstIndex(of: char)?.utf16Offset(in: self)
+    }
+}
+
+public extension String {
+    func indicesOf(string: String) -> [Int] {
+        var indices = [Int]()
+        var searchStartIndex = self.startIndex
+        while searchStartIndex < self.endIndex,
+              let range = self.range(of: string, range: searchStartIndex..<self.endIndex),
+              !range.isEmpty
+        {
+            let index = distance(from: self.startIndex, to: range.lowerBound)
+            indices.append(index)
+            searchStartIndex = range.upperBound
+        }
+        return indices
+    }
+}
+
+public extension RangeReplaceableCollection where Element: Hashable {
+    var squeezed: Self {
+        var set = Set<Element>()
+        return filter{ set.insert($0).inserted }
+    }
+}
+
+extension StringProtocol {
+    var firstUppercased: String { prefix(1).uppercased() + dropFirst() }
+}
