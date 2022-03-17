c43e04f1d074ffb50ae257f8090c61c0603990db
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-17 12:14:10 +0000

    added the ability to detect guessed and actual scores

--- Sources/ScoreFeature/AppViewModel.swift
+++ Sources/ScoreFeature/AppViewModel.swift
@@ -1,6 +1,7 @@
 import ScoreClient
 import Combine
 import WordleKit
+import Helpers
 import SwiftUI
 
 public extension WordleKit {
@@ -8,6 +9,7 @@ public extension WordleKit {
         @Published public var scores = [Scores.Score]()
         @Published private var counter = 0
         private let scoreClient: ScoreClient
+        public var totalScore = 0
         private var cancellables = Set<AnyCancellable>()
         
         public init(scoreClient: ScoreClient) {
@@ -34,6 +36,62 @@ public extension WordleKit {
                     }
                 )
                 .store(in: &cancellables)
+        }
+        
+#warning("Dirty Dancing, figure out a better DSA for this")
+        public func tries(_ score: Scores.Score) -> [Tries] {
+            var tries = [Tries]()
+            var doubleCharCheck: Character = .init(" ")
+            var characterCount = 0
+            
+         
+            
+            score.tries.forEach { stringArray in
+                stringArray.forEach { character in
+                    characterCount += 1
+                    
+                    var t = Tries()
+                    if score.word.contains(character) {
+                        t = Tries(character, 1)
+                    } else {
+                        t = Tries(character, 0)
+                    }
+                    if score.word == stringArray {
+                        t = Tries(character, 2)
+                    } else {
+                        if let index = score.word.firstIndex(of: character),
+                           let mainIndex = stringArray.firstIndex(of: character)  {
+                            if score.word[index] == score.word[mainIndex]  {
+                                if stringArray.indicesOf(string: .init(character)).count >= 2 {
+                                    let s = stringArray.squeezed
+                                    
+                                    if let w = s.firstIndex(of: character) {
+                                        
+                                        if doubleCharCheck == .init(" ") {
+                                            doubleCharCheck = character
+                                            t = Tries(s[w], 2)
+                                            
+                                        }
+                                    }
+                                    
+                                } else {
+                                    t = Tries(character, 2)
+                                }
+                                
+                                
+                            }
+                        }
+                    }
+                    
+                    tries.append(t)
+                }
+            }
+            let count = 30 - characterCount
+       
+            for _ in 0..<count  {
+                tries.append(.init(.init(" ")))
+            }
+            return tries
         }
     }
 }
