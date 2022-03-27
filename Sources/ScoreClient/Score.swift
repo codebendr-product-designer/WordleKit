574351c4bc4ef46d17df0470e3bb0c251517be14
codebendr-product-designer <35074528+codebendr-product-designer@users.noreply.github.com>
2022-03-27 13:22:28 +0000

    added isNew props

--- Sources/ScoreClient/Score.swift
+++ Sources/ScoreClient/Score.swift
@@ -2,6 +2,7 @@ import Foundation
 import WordleKit
 
 public extension WordleKit {
+    
     // MARK: - Scores
     struct Scores: Codable  {
         
@@ -13,14 +14,20 @@ public extension WordleKit {
         
         // MARK: - Score
         public struct Score: Codable, Identifiable, Hashable {
-            public var id: Int = 0
+            public var id: Int
             public var date: String = ""
             public var word: String = ""
             public var tries: [String] = []
+            public var isNew = false
             
-            public init(word: String, tries: [String]) {
+            enum CodingKeys: String, CodingKey {
+                case id, date, word, tries
+            }
+            
+            public init(id: Int = 0, word: String, tries: [String], isNew: Bool = false) {
                 self.word = word
                 self.tries = tries
+                self.id = id
             }
         }
     }
